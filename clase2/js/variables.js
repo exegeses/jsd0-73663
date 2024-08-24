4de0c4b52e871601ef6429a2b8a211d407971dc5
Marcos <exegeses@gmail.com>
2024-08-23 21:07:53 -0300

    Trabajo con archivos externos + Variables y constantes en Java script

---
+++ clase2/js/variables.js
@@ -0,0 +1,16 @@
+/* 
+    Para declarar una variable en Javascript 
+    utilizamos la palabra reservada "let"
+*/
+
+let numero = 0;
+let frase = 'hola mundo desde JavaScript';
+let rojo = 'hsl(0, 50%, 50%)' //'#d00';
+/*
+    Para declarar una constante en Javascript 
+    utilizamos la palabra reservada const
+*/
+const PI = 3.1416;
+// PI = 7; No se puede sobre escribir una constante
+
+document.write(frase);
